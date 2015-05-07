fe12977f8b5fed4d75b49ebbf11ea6256b28e42f
Scott Lee <scott.lee318@gmail.com>
2015-05-07 13:56:46 +0700

    Initial Commit

---
+++ sss-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sss-test
+//
+//  Created by Scott Lee on 5/7/15.
+//  Copyright (c) 2015 Scott Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sss-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sss-test
+//
+//  Created by Scott Lee on 5/7/15.
+//  Copyright (c) 2015 Scott Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
